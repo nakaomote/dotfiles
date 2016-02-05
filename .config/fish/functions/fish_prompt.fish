fcc5aa7b2d40fb2ddf27d0fa9c724e0712102194
William Fletcher <william.fletcher@mail.rakuten.com>
2016-02-05 16:37:40 +0900

    Show which chef server/org is being used

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -8,6 +8,10 @@ function fish_prompt --description 'Write out the prompt'
 
 	set -l normal (set_color normal)
 
+	if set -q -x chef_server
+    set __chef_server (set_color green)"("(set_color $fish_color_host)$chef_server(set_color green)") "
+  end
+
 	# Hack; fish_config only copies the fish_prompt function (see #736)
 	if not set -q -g __fish_classic_git_functions_defined
 		set -g __fish_classic_git_functions_defined
@@ -89,5 +93,5 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+  echo -e -n -s $__chef_server"\n"(set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end
