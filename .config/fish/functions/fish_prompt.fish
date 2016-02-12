35f692c5a5b9533840fa5c524f09d42fa27afc83
William Fletcher <william.fletcher@mail.rakuten.com>
2016-02-12 11:31:10 +0900

    Get the prompt to show which chef server/organization is in use

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -9,7 +9,7 @@ function fish_prompt --description 'Write out the prompt'
 	set -l normal (set_color normal)
 
 	if set -q -x chef_server
-    set __chef_server (set_color green)"("(set_color $fish_color_host)$chef_server(set_color green)") "
+    set __chef_server (set_color green)"("(set_color $fish_color_host)$chef_server(set_color green)") \n"
   end
 
 	# Hack; fish_config only copies the fish_prompt function (see #736)
@@ -93,5 +93,5 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-  echo -e -n -s $__chef_server"\n"(set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+  echo -e -n -s "$__chef_server"(set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
 end
