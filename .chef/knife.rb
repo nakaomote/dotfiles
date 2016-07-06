85a68c27b05c52d54e2b29a2884bebbca06f208a
William Fletcher <william.fletcher@mail.rakuten.com>
2016-07-06 16:04:23 +0900

    Add in a easy to share knife.rb file

---
+++ .chef/knife.rb
@@ -0,0 +1,20 @@
+unless ENV.include?("knife_chef_server")
+    raise "Set chef_server environment variable"
+end
+
+knife_node_name = ENV["knife_node_name"] || ENV["USER"]
+
+log_level                :info
+log_location             STDOUT
+node_name                knife_node_name
+client_key               "#{ENV["HOME"]}/.chef/private.pem"
+chef_server_url          "https://#{ENV["knife_chef_server"]}"
+syntax_check_cache_path  '#{ENV["HOME"]}/.chef/syntax_check_cache'
+cookbook_path            [
+    "#{ENV["HOME"]}/work/my-cookbooks",
+]
+
+knife[:vault_mode]   = 'client'
+knife[:vault_admins] = [
+    knife_node_name,
+                       ]
