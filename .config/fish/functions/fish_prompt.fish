5e59535ecb2ed6e8d634cf4111845d90bf03e4ca
William Fletcher <uiriamu.furecchaa@gmail.com>
2019-05-07 14:36:56 +0900

    Additional fish configuration which likely will not get used

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -93,5 +93,5 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-  echo -e -n -s "$__chef_server"(set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+  echo -e -n -s "$__chef_server"(set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal  $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" '\n' (set_color $color_cwd) (prompt_pwd) $normal ' > '
 end
